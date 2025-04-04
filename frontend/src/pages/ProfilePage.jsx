0a2a16a46b7b9652d213608c784987754d635287
Mohoammad Imran <mohoammadimran@Mohammads-MacBook.local>
2025-04-04 09:06:28 +0600

    project completed initially

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -67,13 +67,13 @@ const ProfilePage = () => {
 
           <div className="space-y-6">
 
-            <div className="space-y-1.5">
+            {/* <div className="space-y-1.5">
               <div className="text-sm text-zinc-400 flex items-center gap-2">
                 <User className="w-4 h-4" />
                 Full Name
               </div>
               <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.fullName}</p>
-            </div>
+            </div> */}
 
             <div className="space-y-1.5">
               <div className="text-sm text-zinc-400 flex items-center gap-2">
